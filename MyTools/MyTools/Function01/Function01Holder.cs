a88cb3aa0ca09b09f1b385240f3ddba69dabca56
himiro7 <hirimo7@gmail.com>
2014-10-06 21:37:20 +0900

    Added Function01Holder.cs

---
+++ MyTools/MyTools/Function01/Function01Holder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BaseControls;
+using BaseControls.ViewModel;
+
+namespace MyTools.Function01
+{
+  public class Function01Holder : DocViewModel
+  {
+    public Function01Holder(MainWindowViewModel mainWindowViewModel) : base(mainWindowViewModel)
+    {
+      Title = "Func01";
+      MyContent = new Function01ViewModel();
+    }
+
+    private Function01ViewModel _MyContent;
+    public Function01ViewModel MyContent
+    {
+      get { return _MyContent; }
+      set
+      {
+        _MyContent = value;
+        RaisePropertyChanged(() => MyContent);
+      }
+    }
+
+  }
+}
